d048031a1bfcd6d934c848b0c296fccbe79ca047
MarianaRomanov <mariana.mariam25@gmail.com>
2021-11-29 03:01:48 +0200

    changes to homework #7 @elpiankova

--- project_7/func_h7.py
+++ project_7/func_h7.py
@@ -1,3 +1,7 @@
+import requests
+import json
+
+
 def is_year_leap(c):
     if (c % 4 == 0 and c % 100 != 0) or c % 400 == 0:
         return True
@@ -22,3 +26,20 @@ def triangle_kind(a, b, c):
             return 'Versatile triangle'
     else:
         return 'Not a triangle'
+
+
+def post_book(base_url, book_data):
+    return requests.post(f'{base_url}books/',
+                         headers={'Content-Type': 'application/json'},
+                         data=json.dumps(book_data))
+
+
+def put_book(base_url, book_id, book_data_changed):
+    return requests.put(f'{base_url}books/{book_id}',
+                        data=book_data_changed)
+
+
+def post_role(base_url, role_data):
+    return requests.post(f'{base_url}roles/',
+                         headers={'Content-Type': 'application/json'},
+                         data=json.dumps(role_data))
--- project_7/task2_homework7.py
+++ project_7/task2_homework7.py
@@ -1,254 +1,150 @@
 import unittest
 import requests
-import json
+from project_7.func_h7 import post_book, put_book, post_role
 
 
-class BookCreateTests(unittest.TestCase):
+class CreateBookTests(unittest.TestCase):
     def setUp(self) -> None:
         self.base_url = 'http://pulse-rest-testing.herokuapp.com/'
+        self.book_data = {'title': 'book1', 'author': 'test1'}
 
     def test_book_create(self):
-        book_data = {'title': 'book1', 'author': 'test1'}
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(book_data))
+        resp = post_book(self.base_url, self.book_data)
         self.assertEqual(resp.status_code, 201)
         self.assertEqual(resp.headers['Content-Type'], 'application/json')
-        body = resp.json()
-        self.assertIn('id', body)
-        self.book_id = body['id']
+        book = resp.json()
+        self.assertIn('id', book)
+        self.book_data['id'] = book['id']
+        self.assertDictEqual(self.book_data, book)
 
     def test_book_create_fail_data(self):
-        book_data = {'title': 'book2'}
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(book_data))
+        self.book_data.pop('author')
+        resp = post_book(self.base_url, self.book_data)
         self.assertNotEqual(resp.status_code, 201)
-        body = resp.json()
-        self.assertNotIn('id', body)
-        self.book_id = None
 
     def tearDown(self) -> None:
-        if self.book_id:
-            requests.delete(f'{self.base_url}/books/{self.book_id}')
+        if 'id' in self.book_data:
+            requests.delete(f'{self.base_url}/books/{self.book_data["id"]}')
 
 
-class BookGetTests(unittest.TestCase):
+class GetBookTests(unittest.TestCase):
     def setUp(self) -> None:
         self.base_url = 'http://pulse-rest-testing.herokuapp.com/'
         self.book_data = {'title': 'book2', 'author': 'test2'}
+        book = post_book(self.base_url, self.book_data).json()
+        self.book_data['id'] = book['id']
 
     def test_get_book(self):
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(self.book_data))
-        self.assertEqual(resp.status_code, 201)
-        body = resp.json()
-        self.assertIn('id', body)
-        self.book_id = body['id']
-        self.book_data['id'] = self.book_id
-        get_book = requests.get(f'{self.base_url}books/{self.book_id}')
+        get_book = requests.get(f'{self.base_url}books/{self.book_data["id"]}')
         self.assertEqual(get_book.status_code, 200)
         self.assertDictEqual(self.book_data, get_book.json())
 
     def test_book_in_the_list(self):
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(self.book_data))
-        self.assertEqual(resp.status_code, 201)
-        body = resp.json()
-        self.assertIn('id', body)
-        self.book_id = body['id']
-        self.book_data['id'] = self.book_id
         get_books = requests.get(f'{self.base_url}books/')
         self.assertTrue(self.book_data in get_books.json())
 
     def tearDown(self) -> None:
-        if self.book_id:
-            requests.delete(f'{self.base_url}/books/{self.book_id}')
+        requests.delete(f'{self.base_url}/books/{self.book_data["id"]}')
 
 
-class BookChangeTests(unittest.TestCase):
+class ChangeBookTests(unittest.TestCase):
     def setUp(self) -> None:
         self.base_url = 'http://pulse-rest-testing.herokuapp.com/'
-        self.book_data = {'title': 'book3', 'author': 'test3'}
+        book_data = {'title': 'book3', 'author': 'test3'}
+        book = post_book(self.base_url, book_data).json()
+        self.book_id = book['id']
 
     def test_change_book(self):
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(self.book_data))
-        self.assertEqual(resp.status_code, 201)
-        body = resp.json()
-        self.assertIn('id', body)
-        self.book_id = body['id']
         book_data_changed = {'title': 'book3edited', 'author': 'test3edited'}
-        change_book = requests.put(f'{self.base_url}books/{self.book_id}',
-                                   data=book_data_changed)
+        change_book = put_book(self.base_url, self.book_id, book_data_changed)
         self.assertEqual(change_book.status_code, 200)
         body_changed = change_book.json()
         self.assertEqual(self.book_id, body_changed['id'])
-        book_data_changed['id'] = body_changed['id']
+        book_data_changed['id'] = self.book_id
         self.assertDictEqual(book_data_changed, body_changed)
 
     def test_change_book_fail_no_book(self):
-        self.book_id = 20
-        book_data_changed = {'title': 'book3edited', 'author': 'test3edited'}
-        change_book = requests.put(f'{self.base_url}books/{self.book_id}',
-                                   data=book_data_changed)
+        book_data_changed = {'id': self.book_id, 'title': '', 'author': ''}
+        change_book = put_book(self.base_url, self.book_id, book_data_changed)
         self.assertNotEqual(change_book.status_code, 200)
-        body_changed = change_book.json()
-        self.assertNotIn('id', body_changed)
 
     def tearDown(self) -> None:
-        if self.book_id:
-            requests.delete(f'{self.base_url}/books/{self.book_id}')
+        requests.delete(f'{self.base_url}/books/{self.book_id}')
 
 
-class BookGetChangedTests(unittest.TestCase):
+class GetChangedBookTests(unittest.TestCase):
     def setUp(self) -> None:
         self.base_url = 'http://pulse-rest-testing.herokuapp.com/'
-        self.book_data = {'title': 'book4', 'author': 'test4'}
-        self.book_data_changed = {'title': 'book4edited', 'author': 'test4edited'}
+        book_data = {'title': 'book4', 'author': 'test4'}
+        self.book = post_book(self.base_url, book_data).json()
+        book_data_changed = {'title': 'book4edited', 'author': 'test4edited'}
+        self.book = put_book(self.base_url, self.book['id'], book_data_changed).json()
 
     def test_get_changed_book(self):
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(self.book_data))
-        self.assertEqual(resp.status_code, 201)
-        body = resp.json()
-        self.assertIn('id', body)
-        self.book_id = body['id']
-        change_book = requests.put(f'{self.base_url}books/{self.book_id}', data=self.book_data_changed)
-        self.assertEqual(change_book.status_code, 200)
-        self.book_data_changed['id'] = self.book_id
-        get_changed_book = requests.get(f'{self.base_url}books/{self.book_id}')
+        get_changed_book = requests.get(f'{self.base_url}books/{self.book["id"]}')
         self.assertEqual(get_changed_book.status_code, 200)
-        self.assertDictEqual(self.book_data_changed, get_changed_book.json())
+        self.assertDictEqual(self.book, get_changed_book.json())
 
     def test_changed_book_in_the_list(self):
-        resp = requests.post(f'{self.base_url}books/',
-                             headers={'Content-Type': 'application/json'},
-                             data=json.dumps(self.book_data))
-        self.assertEqual(resp.status_code, 201)
-        body = resp.json()
-        self.assertIn('id', body)
-        self.book_id = body['id']
-        change_book = requests.put(f'{self.base_url}books/{self.book_id}', data=self.book_data_changed)
-        self.assertEqual(change_book.status_code, 200)
-        self.book_data_changed['id'] = self.book_id
         get_books_with_changed = requests.get(f'{self.base_url}books/')
-        self.assertTrue(self.book_data_changed in get_books_with_changed.json())
+        self.assertTrue(self.book in get_books_with_changed.json())
 
     def tearDown(self) -> None:
-        if self.book_id:
-            requests.delete(f'{self.base_url}/books/{self.book_id}')
+        requests.delete(f'{self.base_url}/books/{self.book["id"]}')
 
 
-class RoleCreateTests(unittest.TestCase):
+class CreateRoleTests(unittest.TestCase):
     def setUp(self):
         self.base_url = 'http://pulse-rest-testing.herokuapp.com/'
+        book_data = {'title': 'book1', 'author': 'test1'}
+        self.book = post_book(self.base_url, book_data).json()
+        self.role_data = {'name': 'role1', 'type': 'test1', 'level': 3, 'book': self.book['id']}
 
     def test_role_create(self):
-        book_data = {'title': 'book1', 'author': 'test1'}
-        resp_book = requests.post(f'{self.base_url}books/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(book_data))
-        self.assertEqual(resp_book.status_code, 201)
-        book_body = resp_book.json()
-        self.assertIn('id', book_body)
-        self.book_id = book_body['id']
-        role_data = {'name': 'role1', 'type': 'test1', 'level': 3, 'book': self.book_id}
-        resp_role = requests.post(f'{self.base_url}roles/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(role_data))
+        resp_role = post_role(self.base_url, self.role_data)
         self.assertEqual(resp_role.status_code, 201)
-        role_body = resp_role.json()
-        self.assertIn('id', role_body)
-        self.role_id = role_body['id']
+        role = resp_role.json()
+        self.assertIn('id', role)
+        self.role_data['id'] = role['id']
+        self.assertDictEqual(role, self.role_data)
 
     def test_role_create_fail_no_book(self):
-        self.book_id = 20
-        role_data = {'name': 'role2', 'type': 'test2', 'level': 2, 'book': self.book_id}
-        resp_role = requests.post(f'{self.base_url}roles/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(role_data))
+        self.role_data['book'] = -1
+        resp_role = post_role(self.base_url, self.role_data)
         self.assertNotEqual(resp_role.status_code, 201)
-        role_body = resp_role.json()
-        self.assertNotIn('id', role_body)
-        self.role_id = None
 
     def test_role_create_fail_data(self):
-        book_data = {'title': 'book1', 'author': 'test1'}
-        resp_book = requests.post(f'{self.base_url}books/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(book_data))
-        book_body = resp_book.json()
-        self.assertIn('id', book_body)
-        self.book_id = book_body['id']
-        role_data = {'type': 'test3', 'level': 3, 'book': self.book_id}
-        resp_role = requests.post(f'{self.base_url}roles/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(role_data))
+        self.role_data.pop('name')
+        resp_role = post_role(self.base_url, self.role_data)
         self.assertNotEqual(resp_role.status_code, 201)
-        role_body = resp_role.json()
-        self.assertNotIn('id', role_body)
-        self.role_id = None
 
     def tearDown(self) -> None:
-        if self.book_id:
-            requests.delete(f'{self.base_url}/books/{self.book_id}')
-        if self.role_id:
-            requests.delete(f'{self.base_url}/roles/{self.role_id}')
+        requests.delete(f'{self.base_url}/books/{self.book["id"]}')
+        if 'id' in self.role_data:
+            requests.delete(f'{self.base_url}/roles/{self.role_data["id"]}')
 
 
-class RoleDeleteTests(unittest.TestCase):
+class DeleteRoleTests(unittest.TestCase):
     def setUp(self):
         self.base_url = 'http://pulse-rest-testing.herokuapp.com/'
-        self.book_data = {'title': 'book2', 'author': 'test2'}
-        self.role_data = {'name': 'role1', 'type': 'test1', 'level': 3}
+        book_data = {'title': 'book2', 'author': 'test2'}
+        self.book = post_book(self.base_url, book_data).json()
+        self.role_data = {'name': 'role1', 'type': 'test1', 'level': 3, 'book': self.book['id']}
+        self.role = post_role(self.base_url, self.role_data).json()
 
     def test_role_delete(self):
-        resp_book = requests.post(f'{self.base_url}books/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(self.book_data))
-        self.assertEqual(resp_book.status_code, 201)
-        book_body = resp_book.json()
-        self.assertIn('id', book_body)
-        self.book_id = book_body['id']
-        self.role_data['book'] = self.book_id
-        resp_role = requests.post(f'{self.base_url}roles/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(self.role_data))
-        self.assertEqual(resp_role.status_code, 201)
-        role_body = resp_role.json()
-        self.assertIn('id', role_body)
-        role_id = role_body['id']
-        delete_role = requests.delete(f'{self.base_url}roles/{role_id}')
+        delete_role = requests.delete(f'{self.base_url}roles/{self.role["id"]}')
         self.assertEqual(delete_role.status_code, 204)
+        self.role_data = None
 
     def test_role_delete_fail_no_role(self):
-        resp_book = requests.post(f'{self.base_url}books/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(self.book_data))
-        self.assertEqual(resp_book.status_code, 201)
-        book_body = resp_book.json()
-        self.assertIn('id', book_body)
-        self.book_id = book_body['id']
-        self.role_data['book'] = self.book_id
-        self.role_data.pop('type')
-        resp_role = requests.post(f'{self.base_url}roles/',
-                                  headers={'Content-Type': 'application/json'},
-                                  data=json.dumps(self.role_data))
-        self.assertNotEqual(resp_role.status_code, 201)
-        role_body = resp_role.json()
-        self.assertNotIn('id', role_body)
-        role_id = None
-        delete_role = requests.delete(f'{self.base_url}roles/{role_id}')
+        delete_role = requests.delete(f'{self.base_url}roles/-1')
         self.assertEqual(delete_role.status_code, 404)
 
     def tearDown(self) -> None:
-        if self.book_id:
-            requests.delete(f'{self.base_url}/books/{self.book_id}')
+        requests.delete(f'{self.base_url}/books/{self.book["id"]}')
+        if self.role_data is not None:
+            requests.delete(f'{self.base_url}/roles/{self.role["id"]}')
 
 
 if __name__ == '__main__':
